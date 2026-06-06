40136dae1a983e0cfb87619c4949d69178760473
theFran21s <fancescorivera2006@gmail.com>
2026-06-06 08:20:05 -0500

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,6 @@
+const boton = document.getElementById("btn");
+const nombre = document.getElementById("nombreCompleto");
+
+boton.addEventListener("click", function() {
+    nombre.style.color = "blue";
+});
